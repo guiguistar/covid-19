a482bf316c7db3a045e821995ebc7f66cab48f79
Guillaume Roux <guillaume.roux.math@free.fr>
2020-09-10 15:07:03 +0200

    L'extraction de données fonctionne. Les valeurs absentes sont remplacées par 0000. Code à nettoyer un peu.

--- dataCatcher/src/dataCatcher/Main.java
+++ dataCatcher/src/dataCatcher/Main.java
@@ -1,6 +1,10 @@
 package dataCatcher;
 
 import java.io.IOException;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import org.jsoup.*;
 import org.jsoup.nodes.*;
@@ -11,30 +15,60 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Bonjour");
 		
-		//doit();
-		testit();
+		HashMap<String, String> datesGoogle = chargerDatesGoogle();
+		ArrayList<String> dates = chargerDates();
+		ArrayList<String> datesManquantes = new ArrayList<String>();
+		
+		
+		for(HashMap.Entry<String, String> date : datesGoogle.entrySet()) {
+			System.out.println(date.getKey() + " " + date.getValue());
+		}
+		
+		for(String date: dates) {
+			System.out.print(date + ",");
+			if(datesGoogle.containsKey(date)) {
+				System.out.println(datesGoogle.get(date));
+			}
+			else {
+				datesManquantes.add(date);
+				System.out.println("0000");
+			}
+		}
 	}
 	public static String parseDate(String date) {
-		String[] mois = {"févr.", "mars", "avr.", "mai", "juin", "juil.", "août"};
-		String[] codes = {"022020", "032020", "042020", "052020", "062020", "072020", "082020"};
+		String[] mois = {"févr.", "mars", "avr.", "mai", "juin", "juil.", "août", "sept."};
+		String[] codes = {"022020", "032020", "042020", "052020", "062020", "072020", "082020", "092020"};
 
-		for(int i = 0; i < 7; i++ ) {
+		for(int i = 0; i < mois.length; i++ ) {
 				date = date.replace(mois[i], codes[i]);
 		}
+
 		return date;
 	}
-	public static void testit() {
-		String date_str = "1 438 le 15 avr.";
+	public static ArrayList<String> chargerDates() {
+		ArrayList<String> dates = new ArrayList<String>();
+		
+		LocalDate dateDebut = LocalDate.of(2020, Month.FEBRUARY, 29);
+		LocalDate dateFin = LocalDate.now();
+		
+		for(LocalDate date = dateDebut; date.isBefore(dateFin); date = date.plusDays(1)) {
+			DateTimeFormatter formateur = DateTimeFormatter.ofPattern("ddMMyyyy");
+			dates.add(date.format(formateur));
+		}
+		
+		return dates;
+	}
+	public static String[] getJetons(String date_str) {
 		date_str = date_str.replaceAll("\\s", "");
 		String[] jetons = date_str.split("le");
-			
-		String num = jetons[0];
-		String date = jetons[1];
 		
-		System.out.println(parseDate(date));
-		System.out.println(num);
+		return jetons;
 	}
-	public static void doit() {
+	public static void testit() {
+
+	}
+	public static HashMap<String, String> chargerDatesGoogle() {
+		HashMap<String, String> dates = new HashMap<String, String>(); 
 		Document doc;
 		Element ol;
 		Elements lis;
@@ -45,12 +79,23 @@ public class Main {
 			
 			String text;
 			for( Element li: lis) {
-				text = li.ownText();
-				System.out.println(text);
+				text = parseDate(li.ownText());
+				
+				String[] jetons = getJetons(text);
+				String num = jetons[0];
+				String date = jetons[1];
+
+				if(date.length() < 8) {
+					date = "0" + date;
+				}
+				
+				dates.put(date, num);
 			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		return dates;
 	}
 }
